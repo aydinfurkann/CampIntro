c05043b48e0245e362d55f10aa6ca1e4af1bfb83
example <email@example.com>
2021-01-23 23:49:22 +0300

    CampIntro

---
+++ KampIntro/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+
+namespace KampIntro
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //type safety - tip güvenliği
+            //Do not repeat yourself - Kendini tekrarlama
+            //Değer tutucu, alias
+            string kategoriEtiketi = "Kategori";
+            int ogrenciSayisi = 32000;
+            double faizOrani = 1.45;
+            bool sistemeGirisYapmisMi = false;
+            double dolarDun = 7.55;
+            double dolarBugun = 7.55;
+
+
+
+            if (dolarDun>dolarBugun)
+            {
+                Console.WriteLine("Azalış butonu");
+            }
+            else if (dolarDun<dolarBugun)
+            {
+                Console.WriteLine("Artış butonu");
+            }
+            else
+            {
+                Console.WriteLine("Değişmedi butonu");
+            }
+
+
+            if (sistemeGirisYapmisMi == true)
+            {
+                Console.WriteLine("Kullanıcı Ayarları butonu");
+            }
+            else
+            {
+                Console.WriteLine("Giriş yap butonu");
+            }
+
+            Console.WriteLine(kategoriEtiketi);
+
+        }
+    }
+}
---
+++ Odev/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace Odev
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            int sayi1 = 10;
+            while (sayi1 <= 30)
+            {
+                Console.WriteLine(sayi1);
+                sayi1 += 1;
+            }
+
+
+
+        }
+    }
+}
+
